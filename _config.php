e2d68ef62f6ad5338b9c9270e187f5a0d15dd28e
nicolaas <nicolaas@7b40dde9-fc51-43d4-b609-63a66f0a0cee>
2014-04-09 04:40:39 +0000

    UPGRADE 3.1
    git-svn-id: http://sunny.svnrepository.com/svn/sunny-side-up-general/termsandconditions/trunk@5587 7b40dde9-fc51-43d4-b609-63a66f0a0cee

--- _config.php
+++ _config.php
@@ -6,8 +6,4 @@
  *
  **/
 
-//copy the lines between the START AND END line to your /mysite/_config.php file and choose the right settings
-//===================---------------- START tersmandconditions MODULE ----------------===================
-//TemplateOverviewPage::set_auto_include(true);
-//===================---------------- END tersmandconditions MODULE ----------------===================
 
